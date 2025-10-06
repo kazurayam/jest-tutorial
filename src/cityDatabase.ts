9f355c82bd522d287fa996907ce501685ebda0a7
kazurayam <kazuaki.urayama@gmail.com>
2025-10-06 18:18:12 +0900

    add src/setup-and-teardown.test.ts; add src/cityDatabase.ts

---
+++ src/cityDatabase.ts
@@ -0,0 +1,11 @@
+export const initializeCityDatabase = () => {
+    return 'initialized City Database';
+}
+
+export const clearCityDatabase = () => {
+    return "cleared City Database";
+}
+
+export function isCity(name: string): boolean {
+    return true;
+}
---
+++ src/setup-and-teardown.test.ts
@@ -0,0 +1,16 @@
+import { initializeCityDatabase, clearCityDatabase, isCity } from './cityDatabase';
+beforeEach(() => {
+    initializeCityDatabase();
+});
+
+afterEach(() => {
+    clearCityDatabase();
+});
+
+test('city database has Vienna', () => {
+    expect(isCity('Vienna')).toBeTruthy();
+})
+
+test('city database has San Juan', () => {
+    expect(isCity('San Juan')).toBeTruthy();
+})
